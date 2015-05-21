06f9176d4b3e9e66617c625807cdaae6143431de
PaulDHawk <hawkpaul@gmail.com>
2015-05-21 14:31:53 -0500

    Initial Commit

---
+++ SimpleCTA/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SimpleCTA
+//
+//  Created by paul hawk on 5/21/15.
+//  Copyright (c) 2015 Depaul University. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
