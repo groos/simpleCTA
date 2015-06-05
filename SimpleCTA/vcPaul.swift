15ffe4ac29c87126a91d0d19cd5684881e3e58b1
PaulDHawk <hawkpaul@gmail.com>
2015-06-05 13:07:35 -0500

    added headers to main vc. todo code it up

--- SimpleCTA/vcPaul.swift
+++ SimpleCTA/vcPaul.swift
@@ -10,34 +10,11 @@ import UIKit
 
 
 
-class vcPaulTest: UIViewController, UITableViewDataSource, UITableViewDelegate {
-    private let routes : [Route]? = DB().dbtest()
+class vcPaulTest: UIViewController {
 
-    //let simpleTableIdentifier = "SimpleTableIdentifier"
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-            if let c = routes?.count { return c }
-            return 0
-    }
-    @IBAction func killButtonPressed(sender: UIButton) {
-        busImage.image = nil
-    }
-    
-    @IBOutlet var busImage: UIImageView!
-    func tableView(tableView: UITableView,
-        cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Red", forIndexPath: indexPath) as! UITableViewCell
-        
-        if let route = routes?[ indexPath.row ] {
-            cell.textLabel!.text = route.routeLongName
-        } else {
-            cell.textLabel!.text = "Error"
-        }
-        return cell
-    }
     
     override func viewWillAppear(animated: Bool) {
-                super.viewWillAppear(animated);
+        super.viewWillAppear(animated);
     }
     
     override func viewDidLoad() {
